e1c4b9a31c318e6c12ea65715b551d5fd243b2a4
Nariman <abonent26pg@mail.ru>
2023-04-16 20:30:13 +0300

    added save data

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,49 +1,74 @@
 import { defineStore } from "pinia";
+import { ref, computed, watch } from "vue";
 
-export const useMovieStore = defineStore("movieStore", {
-  state: () => ({
-    movies: [
-      {
-        filmId: 1,
-        nameEn: "Spider-Man",
-        description:
-          "After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
-        posterUrl:
-          "https://kinopoiskapiunofficial.tech/images/posters/kp/590286.jpg",
-        year: "2002-05-01",
-        isWatched: false,
-      },
-      {
-        filmId: 2,
-        nameEn: "The Batman",
-        description:
-          "In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.",
-        posterUrl:
-          "https://kinopoiskapiunofficial.tech/images/posters/kp/590286.jpg",
-        year: "2022-03-01",
-        isWatched: false,
-      },
-    ],
-    activeTab: 2,
-  }),
-  getters: {
-    watchedMovies() {
-      return this.movies.filter((el) => el.isWatched);
+// export const useMovieStore = defineStore("movieStore", {
+//   state: () => ({
+//     movies: [],
+//     activeTab: 2,
+//   }),
+//   getters: {
+//     watchedMovies() {
+//       return this.movies.filter((el) => el.isWatched);
+//     },
+//     totalCountMovies() {
+//       return this.movies.length;
+//     },
+//   },
+//   actions: {
+//     setActiveTab(id) {
+//       this.activeTab = id;
+//     },
+//     toggleWatched(id) {
+//       const idx = this.movies.findIndex((el) => el.filmId === id);
+//       this.movies[idx].isWatched = !this.movies[idx].isWatched;
+//     },
+//     deleteMovie(id) {
+//       this.movies = this.movies.filter((el) => el.filmId !== id);
+//     },
+//   },
+// });
+
+export const useMovieStore = defineStore("movieStore", () => {
+  const movies = ref([]);
+  const activeTab = ref(2);
+  const moviesInLocalStorage = localStorage.getItem("movies");
+  if (moviesInLocalStorage) {
+    movies.value = JSON.parse(moviesInLocalStorage)._value;
+    console.log(JSON.parse(moviesInLocalStorage));
+  }
+
+  const watchedMovies = computed(() =>
+    movies.value.filter((el) => el.isWatched)
+  );
+
+  const totalCountMovies = computed(() => movies.value.length);
+
+  const setActiveTab = (id) => {
+    activeTab.value = id;
+  };
+  const toggleWatched = (id) => {
+    const idx = movies.value.findIndex((el) => el.filmId === id);
+    movies.value[idx].isWatched = !movies.value[idx].isWatched;
+  };
+  const deleteMovie = (id) => {
+    movies.value = movies.value.filter((el) => el.filmId !== id);
+  };
+
+  watch(
+    () => movies,
+    (state) => {
+      localStorage.setItem("movies", JSON.stringify(state));
     },
-    totalCountMovies() {
-      return this.movies.length;
-    },
-  },
-  actions: {
-    setActiveTab(id) {
-      this.activeTab = id;
-    },
-    toggleWatched(id) {
-      const idx = this.movies.findIndex((el) => el.id === id);
-      this.movies[idx].isWatched = !this.movies[idx].isWatched;
-    },
-    deleteMovie(id) {
-      this.movies = this.movies.filter((el) => el.filmId !== id);
-    },
-  },
+    { deep: true }
+  );
+
+  return {
+    movies,
+    activeTab,
+    watchedMovies,
+    totalCountMovies,
+    setActiveTab,
+    toggleWatched,
+    deleteMovie,
+  };
 });
--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,30 +1,64 @@
 import { defineStore } from "pinia";
+import { useMovieStore } from "./MovieStore";
+import { ref } from "vue";
 import axios from "axios";
 const url =
   "http://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 const api_key = "36bb301d-fadf-4371-b97e-ede492f71f8d";
-export const useSearchStore = defineStore("searchStore", {
-  state: () => ({
-    loader: false,
-    movies: [],
-  }),
-  actions: {
-    async getMovies(search) {
-      this.loader = true;
-      const res = await axios.get(`${url}${search}`, {
-        headers: {
-          "X-API-KEY": api_key,
-        },
-      });
-      const data = await res.data;
-      this.movies = data.films.map((film) => {
-        return {
-          ...film,
-          isWatched: false,
-        };
-      });
-      console.log(this.movies);
-      this.loader = false;
-    },
-  },
+// export const useSearchStore = defineStore("searchStore", {
+//   state: () => ({
+//     loader: false,
+//     movies: [],
+//   }),
+//   actions: {
+//     async getMovies(search) {
+//       this.loader = true;
+//       const res = await axios.get(`${url}${search}`, {
+//         headers: {
+//           "X-API-KEY": api_key,
+//         },
+//       });
+//       const data = await res.data;
+//       this.movies = data.films;
+//       this.loader = false;
+//     },
+
+//     addToUserMovies(obj) {
+//       const movieStore = useMovieStore();
+
+//       movieStore.movies.push({ ...obj, isWatched: false });
+//       movieStore.activeTab = 1;
+//     },
+//   },
+// });
+
+export const useSearchStore = defineStore("searchStore", () => {
+  const loader = ref(false);
+  const movies = ref([]);
+
+  const getMovies = async (search) => {
+    loader.value = true;
+    const res = await axios.get(`${url}${search}`, {
+      headers: {
+        "X-API-KEY": api_key,
+      },
+    });
+    const data = await res.data;
+    movies.value = data.films;
+    loader.value = false;
+  };
+
+  const addToUserMovies = (obj) => {
+    const movieStore = useMovieStore();
+
+    movieStore.movies.push({ ...obj, isWatched: false });
+    movieStore.activeTab = 1;
+  };
+
+  return {
+    loader,
+    movies,
+    getMovies,
+    addToUserMovies,
+  };
 });
